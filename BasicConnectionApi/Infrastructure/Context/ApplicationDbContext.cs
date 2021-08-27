5df273c9999e69d8f9df0c55ccce425e0b8a615a
Marlody <marlodycairo@hotmail.com>
2021-08-27 17:45:30 -0500

    Add project files.

---
+++ BasicConnectionApi/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BasicConnectionApi.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicConnectionApi.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ BasicConnectionApi/Infrastructure/Entities/Value.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicConnectionApi.Infrastructure.Entities
+{
+    public class Value
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Value1 { get; set; }
+        public string Value2 { get; set; }
+    }
+}
